87a5caeb6334642e86fa4adafa940307194c0ffc
SHANKARANARAYANAN S S <usssn43@gmail.com>
2025-12-17 08:25:30 +0530

    Create Relational Operations

---
+++ Operations/Relational.py
@@ -0,0 +1,9 @@
+n1 = float(input("Enter number-1:"))
+n2 = float(input("Enter number-2:"))
+
+print(n1, "\t<\t", n2, "\t:\t",n1<n2)
+print(n1, "\t>\t", n2, "\t:\t",n1>n2)
+print(n1, "\t<=\t", n2, "\t:\t",n1<=n2)
+print(n1, "\t>=\t", n2, "\t:\t",n1>=n2)
+print(n1, "\t==\t", n2, "\t:\t",n1==n2)
+print(n1, "\t!=\t", n2, "\t:\t",n1!=n2)

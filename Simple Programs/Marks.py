4ec10e69651ed86a9a0b7e83f347f5e39f0eaed8
SHANKARANARAYANAN S S <usssn43@gmail.com>
2025-12-23 11:34:09 +0530

    Create Marks

---
+++ Simple Programs/Marks.py
@@ -0,0 +1,9 @@
+m1 = int(input("Enter mark-1:"))
+m2 = int(input("Enter mark-2:"))
+m3 = int(input("Enter mark-3:"))
+m4 = int(input("Enter mark-4:"))
+m5 = int(input("Enter mark-5:"))
+total = m1+m2+m3+m4+m5
+avg = total/5
+print("Total Marks: ",total)
+print("Average Marks: ",avg)

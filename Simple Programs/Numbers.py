108bf696db43cb410c22600af994737cc60d9ed5
SHANKARANARAYANAN S S <usssn43@gmail.com>
2025-12-23 11:35:49 +0530

    Create Numbers

---
+++ Simple Programs/Numbers.py
@@ -0,0 +1,6 @@
+num=int(input("Enter any numbers:"))
+print("Square of Number: ",num**2)
+print("Cube of Number: ",num**3)
+
+#print("Square of Number: ",num*num)
+#print("Cube of Number: ",num*num*num)

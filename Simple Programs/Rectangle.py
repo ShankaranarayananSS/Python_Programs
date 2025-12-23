cd49de4b401470b02dab89ebac9602c60d3be4ca
SHANKARANARAYANAN S S <usssn43@gmail.com>
2025-12-23 11:37:22 +0530

    Create Rectangle

---
+++ Simple Programs/Rectangle.py
@@ -0,0 +1,7 @@
+#Perimeter of Rectangle : 2 * (Length + Breadth)
+#Area of Rectangle      : Length * Breadth
+
+l = float(input("Enter Length of Rectangle"))
+b = float(input("Enter Breadth of Rectangle"))
+print("Perimeter of Rectangle: ",2*(l+b))
+print("Area of Rectangle: ",l*b)

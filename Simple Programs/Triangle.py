bf3a076e680b4c0038fcb809fb1a9cd32d171204
SHANKARANARAYANAN S S <usssn43@gmail.com>
2025-12-23 11:41:03 +0530

    Create Triangle

---
+++ Simple Programs/Triangle.py
@@ -0,0 +1,5 @@
+#Area of Right Angle Triangle : 1/2 * Base * Height
+
+b = float(input("Enter Base of Triangle:"))
+h = float(input("Enter Height of Triangle:"))
+print("Area of Triangle: ",(b*h)/2)

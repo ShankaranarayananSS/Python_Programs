5eb91fd52f2b7c59e45db10661d135cb44fca819
SHANKARANARAYANAN S S <usssn43@gmail.com>
2025-12-23 11:38:35 +0530

    Create Simple Interest

---
+++ Simple Programs/Simple Interest.py
@@ -0,0 +1,6 @@
+#Simple Interest : (Principal Amount * No. of Years * Rate of Interest) / 100
+
+p = float(input("Enter Principal amount:"))
+n = float(input("Enter No. of Years:"))
+r = float(input("Enter Rate of Interest:"))
+print("Simple Interest: ",(p*n*r)/100)

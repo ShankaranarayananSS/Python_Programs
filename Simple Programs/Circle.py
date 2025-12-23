f158f99b0b2bebc55cb4fd09e218b5169f025f32
SHANKARANARAYANAN S S <usssn43@gmail.com>
2025-12-23 11:32:20 +0530

    Create Circle

---
+++ Simple Programs/Circle.py
@@ -0,0 +1,6 @@
+#Circumference of Circle  : 2 * Pi * Radius
+#Area of Circle           : Pi * Radius * Radius
+
+r = float(input("Enter radius of a Circle:"))
+print("Circumference of Circle: ",2*3.14*r)
+print("Area of Circle: ",3.14*r*r)

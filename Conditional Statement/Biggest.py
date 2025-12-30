b8ed09a4be0433d125a70eda67a4764b5179f474
SHANKARANARAYANAN S S <usssn43@gmail.com>
2025-12-30 09:37:28 +0530

    Create Biggest

---
+++ Conditional Statement/Biggest.py
@@ -0,0 +1,8 @@
+n1= int(input("Enter number-1:"))
+n2= int(input("Enter number-2:"))
+if(n1>n2):
+    print(n1," is bigger than ",n2)
+elif(n2>n1):
+    print(n2," is bigger than ",n1)
+else:
+    print("Both are Equal!")

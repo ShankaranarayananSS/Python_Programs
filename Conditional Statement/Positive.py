b4c5847f6beede4e821b3b9bb214fa3b1bb4bba7
SHANKARANARAYANAN S S <usssn43@gmail.com>
2025-12-30 09:40:15 +0530

    Create Positive

---
+++ Conditional Statement/Positive.py
@@ -0,0 +1,7 @@
+num = int(input("Enter any number:"))
+if(num>0):
+    print(num,"is Positive number!")
+elif(num<0):
+    print(num,"is Negative number!")
+else:
+    print(num,"is Either Positive or Negative")

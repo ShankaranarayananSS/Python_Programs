95cbc8387fe2d7190068fc44ecfc400feff9fe4b
SHANKARANARAYANAN S S <usssn43@gmail.com>
2026-01-21 09:03:19 +0530

    Create Swap Two Numbers

---
+++ Simple Programs/Swap.py
@@ -0,0 +1,16 @@
+n1 = int(input("Enter Number 1:"))
+n2 = int(input("Enter Number 1:"))
+
+print("\nNumbers Before Swapping:\nNumber 1:",n1,"\nNumber 2:",n2)
+
+#Swapping without third variable:
+n1,n2 = n2,n1 
+
+print("\nNumbers After Swapping:\nNumber 1:",n1,"\nNumber 2:",n2)
+
+'''
+Swapping using third variable
+temp = n1
+n1 = n2
+n2 = temp
+'''
